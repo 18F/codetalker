3fd3c8830f878a378cbe1724e0ae88e231414f33
DaveCaraway <dave@fogmine.com>
2014-06-09 13:22:20 -0400

    Porting to express
    This commit adds the angular MEAN stack to the project. The system still needs to be ported from RESTify over to express.
    
    SEE https://github.com/18F/codetalker/issues/8

--- server.js
+++ server.js
@@ -1,44 +1,25 @@
-var config      = require('config'),
-    restify     = require('restify'),
-    api         = require('./routes/api'),
-    fs          = require('fs')
+'use strict';
 
-var app = restify.createServer()
+var express = require('express');
 
-app.use(restify.queryParser())
-app.use(restify.CORS())
-app.use(restify.fullResponse())
+/**
+ * Main application file
+ */
 
-// Routes
-app.get('/', function (req, res, next)
-  {
-    var data = fs.readFileSync(__dirname + '/index.html');
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-    res.status(200);
-    res.header('Content-Type', 'text/html');
-    res.end(data.toString().replace(/host:port/g, req.header('Host')));
-  });
+var config = require('./lib/config/config');
 
-app.get('/v0/q', api.v0.query.get)
-app.get('/v0/s', api.v0.search.get)
-app.get('api/v0/q', api.v0.query.get)
-app.get('api/v0/s', api.v0.search.get)
+// Setup Express
+var app = express();
+require('./lib/config/express')(app);
+require('./lib/routes')(app);
 
-app.get('/.well-known/status', function (req, res, next)
-  {
-    var codes_2012 = require(process.cwd() + '/data/codes-2012'),
-        missing_zoos = (codes_2012['712130'] == undefined);
-    
-    var status = {
-            'status': (missing_zoos ? 'Missing data' : 'ok'),
-            'updated': Math.floor((new Date()).getTime() / 1000),
-            'dependencies': null,
-            'resources': null,
-        };
-    
-    res.send(status);
-  });
-
+// Start server
 app.listen(config.port, config.ip, function () {
-  console.log( "Listening on port " + config.port )
-})
+  console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;

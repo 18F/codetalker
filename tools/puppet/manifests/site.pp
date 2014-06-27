65f1466404ad09b6b240f816f24339733a9fd74f
DaveCaraway <dave@fogmine.com>
2014-06-27 09:55:13 -0400

    Removed the .vagrant folder, updated travis build to configure using puppet

--- tools/puppet/manifests/site.pp
+++ tools/puppet/manifests/site.pp
@@ -1,53 +1,7 @@
-#Install Node and NPM
-class { 'nodejs':
-  version => 'stable',
-}->
-package {['grunt-cli', 'forever']:
-  ensure      => present,
-  provider    => 'npm',
-  require     => Class['nodejs'],
-}
+include "./install.pp"
 
-user {'evented':
-  groups => ['sudo'],
-  ensure => present,
-  managehome => true,
-  shell => '/bin/bash',
-}
-
-Exec {
-    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/node/node-default/bin/" ],
-    timeout   => 0,
-}
-
-class { 'apt':
-  always_apt_update    => true,
-  update_timeout       => undef
-}
-
-#Set system timezone to UTC
-class { "timezone":
-  timezone => 'UTC',
-}
-
-package { [
-    'curl',
-    'wget',
-  ]:
-  ensure  => 'installed',
-}
-
-exec { 'install_compass':
-  command   => "gem install compass",
-  unless    => "gem search -i compass",
-}
-->exec { 'install_server':
-  command   => "npm install",
-  cwd       => "/vagrant",
-  require   => Class['nodejs']
-}->
-  exec { 'start':
+exec { 'start':
     command   => "forever start server.js",
     cwd       => "/vagrant",
     unless    => "ps -ef | grep '[f]orever'"
-  }
+}

c0a7d8dc9960d7e0ab605198a0f6cf79d2fb919b
Catherine Devlin <catherine.devlin@gmail.com>
2014-07-18 15:35:43 -0400

    diagnose travis failure with wc and count(*)

--- data/import_data.sh
+++ data/import_data.sh
@@ -1,4 +1,4 @@
 python unify_code_files.py
 ddlgenerator --inserts --drops --force-key --key=id_ postgresql all_codes.json > all_codes.sql
 psql -f all_codes.sql -U postgres codetalker 
-psql -f derived_tables.sql -U postgres codetalker 
+psql -e -f derived_tables.sql -U postgres codetalker 
